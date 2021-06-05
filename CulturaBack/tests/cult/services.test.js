7f9fcae017fa995034383e119a7b844f7a6d1d19
GregoriSimei <gregorisimei@gmail.com>
2021-06-04 21:19:01 -0300

    Test check the period

--- CulturaBack/tests/cult/services.test.js
+++ CulturaBack/tests/cult/services.test.js
@@ -16,22 +16,35 @@ describe('Test Cult Service', ()=>{
                 }
             }
         )
-    });
+    })
 
     it('Create and Save Cult Successfully', async ()=>{
         const cult = {
-            title: "Cult Tests 2",
+            title: "Cult Tests",
             link : "www.teste.com.br",
             date : "2021-03-03",
-            period : "morning",
-            deleted : false
+            period : "morning"
         }
 
         var response = await cultService.save(cult);
 
         expect(response.status).toBe(200);
         expect(response.message).toBe("Success");
-    });
+    })
+
+    it('Unable to create a cult with an unlisted period', async ()=>{
+        const cult = {
+            title: "Cult Test",
+            link: "www.teste.com.br",
+            date: "2021-03-05",
+            period: "not is morning or evening"
+        }
+
+        var response = await cultService.save(cult);
+
+        expect(response.status).toBe(400);
+        expect(response.message).toBe("Period is not valid");
+    })
 
     afterAll(async ()=>{
         const collections = await mongoose.connection.db.collections();
